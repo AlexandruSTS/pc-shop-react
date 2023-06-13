2621db3d15aa730318aa7fad6d41cef70c37c420
Alexandru Anghel <alexandru@ditar.io>
2023-06-13 11:06:42 +0200

    authentication added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import PaginationComponent from "./components/PaginationComponent.tsx";
+import LoginButton from "./LogginButton.tsx";
+import LogoutButton from "./LogoutButton.tsx";
+import {useAuth0} from "@auth0/auth0-react";
 
 function App() {
+    const {user, isAuthenticated, isLoading} = useAuth0();
+
+    // if (isLoading) {
+    //     return <div>En chargement...</div>
+    // }
 
     return (
         <div className='container mt-5'>
-            <h2>Simple Pagination Example in React</h2>
-            <PaginationComponent/>
+            <h2>Items</h2>
+            {isAuthenticated ?
+                <div>
+                    <p> Bonjour {user?.name} <LogoutButton/> </p>
+                    <PaginationComponent/>
+                </div> : <LoginButton/>}
         </div>
     );
 }
